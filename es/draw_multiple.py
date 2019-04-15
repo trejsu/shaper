08a044ce50d3416361144ab9b736fc2c3397ff9e
trejsu <trejsu@gmail.com>
2019-04-15 20:07:47 +0200

    Modifies draw multiple script to parallelize drawing

--- es/draw_multiple.py
+++ es/draw_multiple.py
@@ -1,5 +1,4 @@
 import logging
-import os
 from collections import namedtuple
 
 import numpy as np
@@ -162,8 +161,9 @@ if __name__ == '__main__':
     parser.add_argument("--output-path", type=str, required=True)
     parser.add_argument("--reward", type=str, required=True)
     parser.add_argument("--coeffs", type=str, required=True)
-    parser.add_argument("--part-size", type=int, default=100)
-    parser.add_argument("--num-samples", type=int, default=1000)
+    parser.add_argument("--part-index", type=int, default=0)
+    parser.add_argument("--samples-start", type=int, default=0)
+    parser.add_argument("--samples-end", type=int, default=1000)
 
     args = parser.parse_args()
 
@@ -174,7 +174,8 @@ if __name__ == '__main__':
         X_test = X_test.astype('float32')
         X_test /= 255
         print("Loaded MNIST test data.")
-        return X_test[0:args.num_samples], Y_test[0:args.num_samples]
+        return X_test[args.samples_start:args.samples_end], \
+               Y_test[args.samples_start:args.samples_end]
 
 
     X, Y = data_mnist()
@@ -188,46 +189,15 @@ if __name__ == '__main__':
         rewards_coeffs=args.coeffs
     )
 
-    num_parts = args.num_samples // args.part_size
+    num_samples = args.samples_end - args.samples_start
 
-    for part in tqdm(range(num_parts)):
-        part_start = part * args.part_size
-        part_end = part_start + args.part_size
-
-        part_X = X[part_start:part_end]
-        part_Y = Y[part_start:part_end]
-
-        part_X_drawings = drawer.draw(images=part_X, n=N)
-        assert len(part_X_drawings) == N
-
-        for n in range(1, N + 1):
-            np.savez(
-                args.output_path % n + f'-part-{part}',
-                targets=part_X,
-                drawings=part_X_drawings[n - 1],
-                Y=part_Y
-            )
-
-    print('Drawing complete.')
-    print('Merging parts...')
+    X_drawings = drawer.draw(images=X, n=N)
+    assert len(X_drawings) == N
 
     for n in range(1, N + 1):
-        targets = np.empty((args.num_samples, 28, 28, 1))
-        drawings = np.empty((args.num_samples, 28, 28, 1))
-        Y = np.empty((args.num_samples,))
-
-        for part in range(num_parts):
-            part_start = part * args.part_size
-            part_end = part_start + args.part_size
-
-            part_path = (args.output_path % n) + f'-part-{part}.npz'
-            part_data = np.load(part_path)
-            targets[part_start:part_end] = part_data['targets']
-            drawings[part_start:part_end] = part_data['drawings']
-            Y[part_start:part_end] = part_data['Y']
-
-        np.savez(args.output_path % n, targets=targets, drawings=drawings, Y=Y)
-
-        for part in range(num_parts):
-            part_path = (args.output_path % n) + f'-part-{part}.npz'
-            os.remove(part_path)
+        np.savez(
+            args.output_path % n + f'-part-{args.part_index}',
+            targets=X,
+            drawings=X_drawings[n - 1],
+            Y=Y
+        )
---
+++ experiments/merge_parts.py
@@ -0,0 +1,37 @@
+import os
+
+import numpy as np
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n", type=int, required=True)
+    parser.add_argument("--num-samples", type=int, required=True)
+    parser.add_argument("--num-parts", type=int, required=True)
+    parser.add_argument("--parts-path", type=str, required=True)
+
+    args = parser.parse_args()
+
+    part_size = args.num_samples // args.num_parts
+
+    for n in range(1, args.n + 1):
+        targets = np.empty((args.num_samples, 28, 28, 1))
+        drawings = np.empty((args.num_samples, 28, 28, 1))
+        Y = np.empty((args.num_samples,))
+
+        for part in range(args.num_parts):
+            part_start = part * part_size
+            part_end = part_start + part_size
+
+            part_path = (args.parts_path % n) + f'-part-{part}.npz'
+            part_data = np.load(part_path)
+            targets[part_start:part_end] = part_data['targets']
+            drawings[part_start:part_end] = part_data['drawings']
+            Y[part_start:part_end] = part_data['Y']
+
+        np.savez(args.parts_path % n, targets=targets, drawings=drawings, Y=Y)
+
+        for part in range(args.num_parts):
+            part_path = (args.parts_path % n) + f'-part-{part}.npz'
+            os.remove(part_path)
