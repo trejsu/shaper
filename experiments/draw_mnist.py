8a258e8928f043c2ef02d0762e36ffe118940175
trejsu <trejsu@gmail.com>
2019-04-20 18:24:34 +0200

    Modifies draw mnist script for mnist train set drawing

--- experiments/draw_mnist.py
+++ experiments/draw_mnist.py
@@ -8,8 +8,6 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--output-path", type=str, required=True)
-    parser.add_argument("--reward", type=str, required=True)
-    parser.add_argument("--coeffs", type=str, required=True)
     parser.add_argument("--part-index", type=int, default=0)
     parser.add_argument("--samples-start", type=int, default=0)
     parser.add_argument("--samples-end", type=int, default=1000)
@@ -18,27 +16,23 @@ if __name__ == '__main__':
 
 
     def data_mnist():
-        (_, _), (X_test, Y_test) = mnist.load_data()
-        X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
-        X_test = X_test.astype('float32')
-        X_test /= 255
+        (X, Y), (_, _) = mnist.load_data()
+        X = X.reshape(X.shape[0], 28, 28, 1)
+        X = X.astype('float32')
+        X /= 255
         print("Loaded MNIST test data.")
-        return X_test[args.samples_start:args.samples_end], \
-               Y_test[args.samples_start:args.samples_end]
+        return X[args.samples_start:args.samples_end], \
+               Y[args.samples_start:args.samples_end]
 
 
     X, Y = data_mnist()
-    N = 20
+    N = 100
 
     drawer = Drawer(
         alpha=0.7,
-        background='00',
-        save_all=True,
-        rewards=args.reward,
-        rewards_coeffs=args.coeffs
+        background=None,
+        save_all=True
     )
-
-    num_samples = args.samples_end - args.samples_start
 
     X_drawings = drawer.draw(images=X, n=N)
     assert len(X_drawings) == N
