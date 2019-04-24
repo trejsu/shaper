22be269ef74e819efa730565c492d703054947c8
trejsu <trejsu@gmail.com>
2019-04-24 10:36:26 +0200

    Modifies merge parts script for save all = False case

--- experiments/merge_parts.py
+++ experiments/merge_parts.py
@@ -15,7 +15,28 @@ if __name__ == '__main__':
 
     part_size = args.num_samples // args.num_parts
 
-    for n in range(1, args.n + 1):
+    if '%d' in args.parts_path:
+        for n in range(1, args.n + 1):
+            targets = np.empty((args.num_samples, 28, 28, 1))
+            drawings = np.empty((args.num_samples, 28, 28, 1))
+            Y = np.empty((args.num_samples,))
+
+            for part in range(args.num_parts):
+                part_start = part * part_size
+                part_end = part_start + part_size
+
+                part_path = (args.parts_path % n) + f'-part-{part}.npz'
+                part_data = np.load(part_path)
+                targets[part_start:part_end] = part_data['targets']
+                drawings[part_start:part_end] = part_data['drawings']
+                Y[part_start:part_end] = part_data['Y']
+
+            np.savez(args.parts_path % n, targets=targets, drawings=drawings, Y=Y)
+
+            for part in range(args.num_parts):
+                part_path = (args.parts_path % n) + f'-part-{part}.npz'
+                os.remove(part_path)
+    else:
         targets = np.empty((args.num_samples, 28, 28, 1))
         drawings = np.empty((args.num_samples, 28, 28, 1))
         Y = np.empty((args.num_samples,))
@@ -24,14 +45,14 @@ if __name__ == '__main__':
             part_start = part * part_size
             part_end = part_start + part_size
 
-            part_path = (args.parts_path % n) + f'-part-{part}.npz'
+            part_path = f'{args.parts_path}-part-{part}.npz'
             part_data = np.load(part_path)
             targets[part_start:part_end] = part_data['targets']
             drawings[part_start:part_end] = part_data['drawings']
             Y[part_start:part_end] = part_data['Y']
 
-        np.savez(args.parts_path % n, targets=targets, drawings=drawings, Y=Y)
+        np.savez(args.parts_path, targets=targets, drawings=drawings, Y=Y)
 
         for part in range(args.num_parts):
-            part_path = (args.parts_path % n) + f'-part-{part}.npz'
+            part_path = f'{args.parts_path}-part-{part}.npz'
             os.remove(part_path)
